d9f3138b668d2809fcf75d3c609fec9f5e8b26e2
Martins <victorcostamartins0@gmail.com>
2025-01-07 23:57:00 -0300

    projeto Hora certa

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("oi")
+const btBack = document.querySelector("#btBack")
+
+const hora = document.querySelector("#hora")
+const min = document.querySelector("#min")
+const seg = document.querySelector("#seg")
+
+btBack.addEventListener("click", () => {
+    document.body.style.backgroundColor="red"
+    // if(document.style)
+})
+
+setInterval(() => {
+    const horaCerta = () => {
+        const horaAtual = new Date();
+        const newHora = horaAtual.getHours()
+        const newMin = horaAtual.getMinutes()
+        const newSeg = horaAtual.getSeconds()
+    
+        hora.innerHTML = newHora
+        min.innerHTML = newMin
+        seg.innerHTML = newSeg
+    }
+
+    return horaCerta()
+}, 1000)
