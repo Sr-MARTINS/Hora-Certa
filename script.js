94771af2b79809e2fbd035c55a67559e32c9b39f
Martins <victorcostamartins0@gmail.com>
2025-01-11 22:57:59 -0300

    atualização de elementos

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-const btBack = document.querySelector("#btBack")
+const box = document.querySelector(".container")
+const btBack = document.querySelector(".btBack")
+const btTela = document.querySelector("#inTela")
 
 const hora = document.querySelector("#hora")
 const min = document.querySelector("#min")
 const seg = document.querySelector("#seg")
 
+const day = document.querySelector("#dia")
+const month = document.querySelector("#mes")
+const year = document.querySelector("#ano")
+
 btBack.addEventListener("click", () => {
-    document.body.style.backgroundColor="red"
-    // if(document.style)
+    if(box.classList.contains('hidden')) {
+        box.classList.remove('hidden')
+        document.body.style.background = "#fff"
+        document.body.style.color = "#212529"
+        btBack.style.color = "#000"
+    }else {
+        box.classList.add('hidden')
+        document.body.style.background = "#06080F"
+        document.body.style.color = "#fff"
+        btBack.style.color = "#fff"
+    }   
+})
+
+btTela.addEventListener("click", () => {
+    console.log("oi")
 })
 
 setInterval(() => {
@@ -19,7 +38,28 @@ setInterval(() => {
         hora.innerHTML = newHora
         min.innerHTML = newMin
         seg.innerHTML = newSeg
+    
+        if(newHora <= 9 ) {
+            hora.innerHTML = "0" + newHora
+        }
+        if(newMin <= 9 ) {
+            min.innerHTML = "0" + newMin
+        }
+        if(newSeg <= 9 ) {
+            seg.innerHTML = "0" + newSeg
+        }
     }
 
     return horaCerta()
 }, 1000)
+
+const calend = () => {
+    const dataAtua = new Date();
+    
+    const newDia = dataAtua.getDay();
+    const newMes = dataAtua.getMonth();
+    const newAno = dataAtua.getFullYear()
+    
+    console.log(newAno,newDia, newMes)
+}
+calend()
